048860829f52d698a5de1e7c7e50aae13c4d2d30
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-05-14 09:13:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+/*
+Given a string, return a string length 2 made of its first 2 chars. 
+If the string length is less than 2, use '@' for the missing chars.
+
+Examples
+
+atFirst('hello') → he
+atFirst('hi') → hi
+atFirst('h') → h@
+
+
+*/
+
+function atFirst(str){
+
+}
+
+
+/*
+Logic-1 -- cigarParty
+When squirrels get together for a party,
+ they like to have cigars. 
+ A squirrel party is successful when the number of cigars is between 40 and 60, 
+ inclusive. Unless it is the weekend, in which case there is no upper bound 
+ on the number of cigars. Return true if the party with the 
+ given values is successful, or false otherwise.
+
+ Examples
+
+cigarParty(30, false) → false
+cigarParty(50, false) → true
+cigarParty(70, true) → true
+*/
+
+function cigarParty(cigars, isWeekend){
+
+}
+
+
+/*
+Logic-1 -- squirrelPlay
+The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean isSummer, return true if the squirrels play and false otherwise.
+
+Examples
+
+squirrelPlay(70, false) → true
+squirrelPlay(95, false) → false
+squirrelPlay(95, true) → true
+*/
+
+function squirrelPlay(temp, isSummer){
+
+}
+
+/*
+Logic-1 -- answerCell
+Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you only answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
+
+Examples
+
+answerCell(false, false, false) → true
+answerCell(false, false, true) → false
+answerCell(true, false, false) → false
+
+*/
+
+function answerCell(isMorning, isMom, isAsleep){
+    
+}
+
+/*
+String-1 -- firstHalf
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+Examples
+
+firstHalf('WooHoo') → WooHoo
+firstHalf('HelloThere') → HelloThere
+firstHalf('abcdefg') → abcdefg
+*/
+
+function firstHalf(str){
+    
+}
